2358a455b6545b630ab9ed31b40b92d9088aaad8
Ben Arthur <bjarthur70@gmail.com>
2025-12-18 15:50:43 -0500

    support multiple microphones

--- src/Ax2.jl
+++ src/Ax2.jl
@@ -12,43 +12,55 @@ load_recording(wavfile) = wavread(wavfile)
 
 Ys_cache = LRU{Tuple{UInt64,Int,Int,Int},DSP.Periodograms.Spectrogram}(maxsize=10)
 
-function calculate_hanning_spectrograms(y, nffts, noverlaps, offset, fs)
-    Ys = Vector{DSP.Periodograms.Spectrogram}(undef, length(nffts))
-    Threads.@threads :greedy for (i,(nfft,noverlap)) in enumerate(zip(nffts,noverlaps))
-        Ys[i] = get!(Ys_cache, (hash(y[1+offset:end,1]), nfft,offset,noverlap)) do
-            spectrogram(y[1+offset:end,1], nfft, noverlap; fs=fs, window=hanning)
+"""
+    calculate_hanning_spectrograms(y, nffts, noverlaps, fs, offset)
+
+`y` has # time samples rows and # microphones columns.  `nffts` and `noverlaps`
+are vectors of the same length.  `fs` is the sampling rate.  `offset` is just
+used to debug.
+
+returned is a matrix of spectrograms with # nffts rows and # mics columns
+"""
+function calculate_hanning_spectrograms(y, nffts, noverlaps, fs, offset)
+    Ys = Matrix{DSP.Periodograms.Spectrogram}(undef, length(nffts), size(y,2))
+    Threads.@threads :greedy for I in CartesianIndices((length(nffts), size(y,2)))
+        infft, imic = I[1], I[2]
+        Ys[infft,imic] = get!(Ys_cache, (hash(y[1+offset:end,imic]), nffts[infft], noverlaps[infft], offset)) do
+            spectrogram(y[1+offset:end,imic], nffts[infft], noverlaps[infft]; fs=fs, window=hanning)
         end
     end
     return Ys
 end
 
 dB = x->20*log10.(x)
-overlay(Ys::Vector{<:DSP.Periodograms.Spectrogram}) = overlay(power.(Ys), dB)
+overlay(Ys::Matrix{<:DSP.Periodograms.Spectrogram}) = overlay(power.(Ys), dB)
 overlay(Ys) = overlay(Ys, identity)
-function overlay(Ys, f)
+function overlay(Ys, fun)
     ntime, nfreq = size.(Ys,2), size.(Ys,1)
     mintime = minimum(round.(Int, maximum(ntime) ./ ntime) .* ntime)
     minfreq = minimum(round.(Int, maximum(nfreq) ./ nfreq) .* nfreq)
-    Y_overlay = zeros(Float32, 4, minfreq, mintime)
-    Threads.@threads :greedy for (icolor, Yi) in enumerate(Ys)
-        sz = size(Yi)
+    Y_overlay = zeros(Float32, 4, minfreq, mintime, size(Ys,2))
+    Threads.@threads :greedy for I in CartesianIndices(size(Ys))
+        infft, imic = I[1], I[2]
+        sz = size(Ys[infft,imic])
         scale = round.(Int, (minfreq, mintime) ./ sz)
         for f0 in 1:scale[1]
             fdelta = sz[1] - length(f0:scale[1]:minfreq)
             for t0 in 1:scale[2]
                 tdelta = sz[2] - length(t0:scale[2]:mintime)
-                Y_overlay[icolor,
+                Y_overlay[infft,
                   f0 : scale[1] : end,
-                  t0 : scale[2] : end] .= f.(@view Yi[1:end-fdelta, 1:end-tdelta])
+                  t0 : scale[2] : end,
+                  imic] .= fun.(@view Ys[infft,imic][1:end-fdelta, 1:end-tdelta])
             end
         end
     end
-    Y_overlay[4,:,:] .= 1
-    if length(Ys) == 2
-        Y_overlay[3,:,:] .= @view Y_overlay[2,:,:]
-        Y_overlay[2,:,:] .= @view Y_overlay[1,:,:]
-    elseif length(Ys) == 1
-        Y_overlay[2,:,:] .= Y_overlay[3,:,:] .= @view Y_overlay[1,:,:]
+    Y_overlay[4,:,:,:] .= 1
+    if size(Ys,1) == 2
+        Y_overlay[3,:,:,:] .= @view Y_overlay[2,:,:,:]
+        Y_overlay[2,:,:,:] .= @view Y_overlay[1,:,:,:]
+    elseif size(Ys,1) == 1
+        Y_overlay[2,:,:,:] .= Y_overlay[3,:,:,:] .= @view Y_overlay[1,:,:,:]
     end
     return Y_overlay
 end
@@ -62,26 +74,37 @@ function precompute_config(nfft, nw, k, fs)
     end
 end
 
+"""
+    calculate_multitaper_spectrograms(y, nffts, noverlaps, nw, k, fs, iclip, output=stderr)
+
+`y` has # time samples rows and # microphones columns.  `nffts` and `noverlaps`
+are vectors of the same length.  `nw` and `k` are the time-bandwidth and number
+of tapers, respectively.  `fs` is the sampling rate.  `iclip` is the portion of
+time to analyse.
+
+returned is a vector for each nfft, each element being a matrix of multi-taper
+periodograms with # time slices rows and # mics columns
+"""
 function calculate_multitaper_spectrograms(y, nffts, noverlaps, nw, k, fs, iclip, output=stderr)
-    function _mt_pgram(idx, nfft, config)
-        _y = y[idx:idx+nfft-1]
+    function _mt_pgram(idx, imic, nfft, config)
+        _y = y[idx:idx+nfft-1, imic]
         _y .-= mean(_y)
         mt_pgram(_y, config)
     end
 
-    mtspectrums = []
+    mtspectrums = Matrix{Periodograms.PeriodogramF}[]
     for (nfft,noverlap) in zip(nffts,noverlaps)
         haskey(configs, nfft) || precompute_config(nfft, nw, k, fs)
         idxs = iclip[1] : nfft-noverlap : iclip[2]+1-nfft+1
-        mtspectrum = Vector{Periodograms.PeriodogramF}(undef, length(idxs))
+        mtspectrum = Matrix{Periodograms.PeriodogramF}(undef, length(idxs), size(y,2))
         i_idxs = Channel() do chnl
             foreach(i_idx->put!(chnl,i_idx), enumerate(idxs))
         end
         @sync for _ in 1:Threads.nthreads()
             Threads.@spawn begin
                 config = take!(configs[nfft])
-                for (i,idx) in i_idxs
-                    mtspectrum[i] = _mt_pgram(idx, nfft, config)
+                for (i,idx) in i_idxs, imic in axes(y,2)
+                    mtspectrum[i,imic] = _mt_pgram(idx, imic, nfft, config)
                 end
                 put!(configs[nfft], config)
             end
@@ -91,19 +114,26 @@ function calculate_multitaper_spectrograms(y, nffts, noverlaps, nw, k, fs, iclip
     return mtspectrums
 end
 
+"""
+returned is a matrix of multi-taper spectrograms with # nffts rows and # mics columns
+"""
 function coalesce_multitaper_power(mts)
-    Y_MTs = DSP.Periodograms.Spectrogram[]
-    for mt in mts
-        p = hcat((power(x) for x in mt)...)
-        push!(Y_MTs, DSP.Periodograms.Spectrogram(p, 0:0., 0:0.))
+    Y_MTs = Matrix{DSP.Periodograms.Spectrogram}(undef,
+            length(mts), size(mts[1],2))
+    for infft in 1:length(mts), imic in axes(mts[infft],2)
+        p = hcat((power(x) for x in mts[infft][:,imic])...)
+        Y_MTs[infft,imic] = DSP.Periodograms.Spectrogram(p, 0:0., 0:0.)
     end
     return Y_MTs
 end
 
+"""
+returned is a matrix of multi-taper F-tests with # nffts rows and # mics columns
+"""
 function coalesce_multitaper_ftest(mts)
-    Fs = Matrix{Float64}[]
-    for mt in mts
-        push!(Fs, hcat((Fpval(x) for x in mt)...))
+    Fs = Matrix{Matrix{Float64}}(undef, length(mts), size(mts[1],2))
+    for infft in 1:length(mts), imic in axes(mts[infft],2)
+        Fs[infft,imic] = hcat((Fpval(x) for x in mts[infft][:,imic])...)
     end
     return Fs
 end
@@ -138,44 +168,45 @@ function refine_ftest(_Fs, minpix1, pval, anyall, sigonly,
         Fs = _Fs
     end
     F_overlay = overlay(Fs)
-    for j in axes(F_overlay,2)
-        Threads.@threads for k in axes(F_overlay,3)
-            if anyall(x->x<pval, view(F_overlay,1:3,j,k))
-                F_overlay[:,j,k] .= 1   # fuchsia
-                F_overlay[2,j,k] = 0
-            elseif sigonly
-                F_overlay[:,j,k] .= 0   # black
-                F_overlay[4,j,k] = 1
+    Threads.@threads for imic in axes(Fs,2)
+        for ifreq in axes(F_overlay,2)
+            for itime in axes(F_overlay,3)
+                if anyall(x->x<pval, view(F_overlay,1:3,ifreq,itime,imic))
+                    F_overlay[:,ifreq,itime,imic] .= 1   # fuchsia
+                    F_overlay[2,ifreq,itime,imic] = 0
+                elseif sigonly
+                    F_overlay[:,ifreq,itime,imic] .= 0   # black
+                    F_overlay[4,ifreq,itime,imic] = 1
+                end
+            end
+        end
+        if sigonly
+            if morphclose
+                F_padded, ipad = get_pad(pad, strelclose, F_overlay[:,:,:,imic], 0)
+                F_closed = closing(F_padded, strelclose)
+                F_overlay[:,:,:,imic] .= reshape(F_closed, 1, size(F_closed)...)[ipad...]
+                F_overlay[2,:,:,imic] .= 0
+                F_overlay[4,:,:,imic] .= 1
+            end
+            if morphopen
+                F_padded, ipad = get_pad(pad, strelopen, F_overlay[:,:,:,imic], 1)
+                F_opened = opening(F_padded, strelopen)
+                F_overlay[:,:,:,imic] .= reshape(F_opened, 1, size(F_opened)...)[ipad...]
+                F_overlay[2,:,:,imic] .= 0
+                F_overlay[4,:,:,imic] .= 1
+            end
+            if minpix2>0
+                labels = label_components(F_overlay[1,:,:,imic], trues(3,3))
+                indices = component_indices(labels)
+                for idx in Iterators.filter(x->length(x) < minpix2, Iterators.drop(indices, 1))
+                    view(F_overlay, 1,:,:,imic)[idx] .= 0
+                    view(F_overlay, 2,:,:,imic)[idx] .= 0
+                    view(F_overlay, 3,:,:,imic)[idx] .= 0
+                end
             end
         end
     end
-    if sigonly
-        if morphclose
-            F_padded, ipad = get_pad(pad, strelclose, F_overlay, 0)
-            F_closed = closing(F_padded, strelclose)
-            F_overlay .= reshape(F_closed, 1, size(F_closed)...)[ipad...]
-            F_overlay[2,:,:] .= 0
-            F_overlay[4,:,:] .= 1
-        end
-        if morphopen
-            F_padded, ipad = get_pad(pad, strelopen, F_overlay, 1)
-            F_opened = opening(F_padded, strelopen)
-            F_overlay .= reshape(F_opened, 1, size(F_opened)...)[ipad...]
-            F_overlay[2,:,:] .= 0
-            F_overlay[4,:,:] .= 1
-        end
-        if minpix2>0
-            labels = label_components(F_overlay[1,:,:], trues(3,3))
-            indices = component_indices(labels)
-            for idx in Iterators.filter(x->length(x) < minpix2, Iterators.drop(indices, 1))
-                view(F_overlay, 1,:,:)[idx] .= 0
-                view(F_overlay, 2,:,:)[idx] .= 0
-                view(F_overlay, 3,:,:)[idx] .= 0
-            end
-        end
-    end
-    F_converted = N0f8.(F_overlay)
-    dropdims(collect(reinterpret(RGBA{N0f8}, F_converted)), dims=1)
+    return collect(dropdims(reinterpret(RGBA{N0f8}, N0f8.(F_overlay)), dims=1))
 end
 
 function scale_and_color(Y_overlay)
@@ -186,8 +217,8 @@ function scale_and_color(Y_overlay)
     collect(dropdims(reinterpret(RGBA{N0f8}, Y_scaled), dims=1)')
 end
 
-function cumpower(p,d)
-    x = dropdims(sum(p, dims=d), dims=d)
+function cumpower(d,p...)
+    x = dropdims(sum(reduce(.+,p,init=p[1]*0.0), dims=d), dims=d)
     @. red(x) + green(x) + blue(x)
 end
 
